5aece3fe40fd6c39b38bab734cb94a1cb1097da1
Gacnik <ales.gacnik@gmail.com>
2023-05-28 15:52:33 +0200

    Commit second version of the program with improvement to the game. Also README.md update

---
+++ DiceGame_v2/main.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+        ofstream file;  //create variable for saving file
+        file.open("results.txt");   //decalre file name for saving
+        int number_of_tries = 10;   
+        int start = 0;  
+        while(start < number_of_tries){
+            srand((unsigned) time(NULL)*(start*time(NULL))); //random number genartor seed value
+            int work_done[6] {0}; //array for storing work done from workers
+            int tabela[6][10][2] {0}; //array for storing work proces
+            file << "Attempt " << start+1 << "\n\n";
+            file << " DAY |   A | wpA |   B|  wpB |   C | wpC |   D | wpD |   E | wpE |   F | TOTAL \n";
+            file << "-------------------------------------------------------------------------------------------\n"; 
+            for(int i = 0; i<10; ++i){ //10 day work proces loop
+                if(i == 9){
+                    file << "  " << i+1 << " ";
+                }else{
+                    file << "   " << i+1 << " ";
+                }
+     
+                if(i == 0){ //first day values generator
+                    int day1 = (rand() % 6) + 1; //random number generator
+                    work_done[0] = day1;
+                    tabela[0][i][0] = day1; //random generate number from 1-6 for the first worker
+                    tabela[0][i][1] = day1; //work progres left by the first worker
+                    for(int k = 1; k<6; ++k){ //loop for generatin number and work progres for other workers
+                        int dice_throw {(rand() % 6) + 1}; //random number generator
+                        tabela[k][i][0] = dice_throw; //storing random number to worker
+                        if(dice_throw > tabela[k-1][i][1]){ //saving if work progres from prevoius is less than generated number
+                            tabela[k][i][1] = tabela[k-1][i][1]; 
+                            work_done[k] = tabela[k-1][i][1]; //saving work done by worker
+                            tabela[k-1][i][1] = 0;
+                        }else{                              // saving if work progres from previous is more than generated number
+                            work_done[k] = dice_throw;        //saving work done by worker
+                            tabela[k-1][i][1] -= tabela[k][i][0];
+                            tabela[k][i][1] = tabela[k][i][0]; 
+                        }
+                    }
+                    for(int k = 0; k<6; ++k){   //file output loop
+                        if(tabela[k][i][0] > 9){
+                            file << "|  " << tabela[k][i][0] << " |";
+                        }else{
+                            file << "|   " << tabela[k][i][0] << " |";
+                        }
+                        if(tabela[k][i][1] > 9){
+                            file << "  " << tabela[k][i][1] << " ";
+                        }else{
+                            file << "   " << tabela[k][i][1] << " ";
+                        }
+                    }
+                    file << "\n";
+                }else{
+                    for(int k = 0 ; k<6;++k){ //value generator for the next 9 days (2-10)
+                        int die_throw {rand()%6+1}; //random number generator
+                        if(k==0){   //work done by first worker (A)
+                            work_done[0] += die_throw; //saving work done by worker
+                            tabela[k][i][0] = die_throw; //storing random number to worker
+                            tabela[k][i][1] = die_throw + tabela[k][i-1][1];
+                        }else{      // work done for the rest of the workers
+                            tabela[k][i][0] = die_throw; //storing random number to worker
+                            if(die_throw > tabela[k-1][i][1]){ //saving if work progres from prevoius is less than generated number
+                                work_done[k] += tabela[k-1][i][1]; //saving work done by worker
+                                tabela[k][i][1] = tabela[k-1][i][1]+tabela[k][i-1][1];     
+                                tabela[k-1][i][1] = 0;
+                            }else{                              // saving if work progres from previous is more than generated number
+                                work_done[k] += die_throw; //saving work done by worker
+                                tabela[k-1][i][1] -= tabela[k][i][0]; 
+                                tabela[k][i][1] = tabela[k][i][0]+tabela[k][i-1][1]; 
+                            }
+                        }
+                    }
+                    for(int k = 0; k<6; ++k){   //print to file loop
+                        if(tabela[k][i][0] > 9){
+                            file << "|  " << tabela[k][i][0] << " |";
+                        }else{
+                            file << "|   " << tabela[k][i][0] << " |";
+                        }
+                        if(tabela[k][i][1] > 9){
+                            file << "  " << tabela[k][i][1] << " ";
+                        }else{
+                            file << "   " << tabela[k][i][1] << " ";
+                        }
+                    }
+                    file << "\n";
+                }
+            }
+            file << "-------------------------------------------------------------------------------------------\n";
+            file << "Skupaj je bilo narejenih: " << tabela[5][9][1] << " izdelkov.\n\n"; 
+            file << " Worker    |   A |   B |   C |   D |   E |   F \n";
+            file << "-----------------------------------------------\n";
+            file << " Work Done ";
+            for(int j = 0 ; j < 6 ; ++j){ //print to file for work done by workers
+                if(work_done[j] > 9){
+                    file << "|  " << work_done[j] << " ";
+                }else{
+                    file << "|   " << work_done[j] << " ";
+                }
+            }
+            file <<"\n\n";
+            file << "*******************************************************************************************\n";
+            file <<"\n\n";
+            start++;
+        }
+        return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 Game with Dice
 
+In this game we have 6 workers (A, B, C, D, E, F).
+Workers work in chain one after another.
+The amount of work thay can do is generate with 1d6 throw,
+(randomly generated number from 1-6 in code). The work that workers do is stored 
+in their work in progress storage for the first five workers(wpA, wpB, wpC, wpD, wpE).
+The work from the last worker (F) is stored in TOTAL items that they build. First 
+worker (A), can always make work per his dice throw. Other workers can make the work 
+per their dice throw but only, if the storake in work in progress is big enough.
+Othervase they can only take what is in the work in progress from previous worker.
+
+The work proces lasts 10 day. Work in progress storage is transfered form prevous day.
+If they want to have succesful 10 day thay have to make 30 parts at the end. of day 10.
+
+---------------------------------------------------------------------------------------
+
+My program (./DiceGame_v2/main.cpp) generated 10 random games and after runing the 
+program a couple of times, the number was alway lower than 30. We are limited by the
+1d6 throw. With max number of 6, we would have to have luck to generate final score of
+30 or more. (Results are in ./results.txt)
+
+In my second program (./DiceGame_v2_improvement), the throws are as follows:
+A & F = 1d20, B & E = 1d10, C & D = 1d6. With this we have a chance to win, but win
+percantage was stil not 100% ( not even 90 %). (Results are in 
+./results_improvement.txt)
+
+
+
+
